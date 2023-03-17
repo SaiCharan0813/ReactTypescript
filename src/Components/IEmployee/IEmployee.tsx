8d38f5aec7ae1e9d31206df737321e31d7793a87
Saicharan0813 <saicharan.r@technovert.com>
2023-03-17 18:23:36 +0530

    Commit1

--- src/Components/IEmployee/IEmployee.tsx
+++ src/Components/IEmployee/IEmployee.tsx
@@ -1,14 +1,15 @@
 export default interface IEmployee {
     [x: string]: any;
-    _id: string;
-    Employee_Img: any
-    First_Name: string;
-    Last_Name: string;
-    Preffered_Name: string;
-    Title: string;
-    Office_Details: string;
-    Dept_Name: string
-    Skype_Id: string;
-    Email_id: string;
-    phone_no: string;
+    employeeId: string;
+    firstName: string;
+    lastName: string;
+    prefferedName: string; 
+    email: string;
+    jobTitleId: string;
+    officeId: string;
+    departmentId: string;
+    phoneNumber: string;
+    skypeId: string;
+    image: any
+
 }
